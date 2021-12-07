b29c03ca2d5542ea6eff3f6d1f7df43655643641
jacopo <jacopok@gmail.com>
2021-12-07 18:52:42 +0100

    Added h5py usage

--- mlgw_bns/dataset_generation.py
+++ mlgw_bns/dataset_generation.py
@@ -1,8 +1,25 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+import h5py
+
 
 class Dataset:
-    pass
+    # saving to file to be managed with https://docs.h5py.org/en/stable/quick.html
+
+    def __init__(self, name):
+        """
+        Initialize dataset.
+        """
+
+        self.name = name
+
+    def save(self) -> None:
+        pass
+
+    def load(self) -> None:
+        with h5py.File(self.name, "r") as file:
+            self.frequencies = file["data"]
 
 
 class SlowWaveformGenerator(ABC):
