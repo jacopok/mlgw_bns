df6e3fbce3c9ff7657a085c44a068b086f1956ab
jacopo <jacopok@gmail.com>
2021-12-14 19:03:51 +0100

    Added "source" button on docs

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx_rtd_theme",
     "myst_parser",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
