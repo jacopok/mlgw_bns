f55e5b13450f1bef8fc301a1e862cbf1a9303338
jacopo <jacopok@gmail.com>
2021-12-21 21:23:50 +0100

    Removed unused code

--- mlgw_bns/taylorf2.py
+++ mlgw_bns/taylorf2.py
@@ -63,41 +63,6 @@ def compute_delta_lambda(m1: float, m2: float, l1: float, l2: float):
 
 
 @njit(cache=True)
-def PhifT6PN(
-    f: np.ndarray, M: float, eta: float, Lam1: float, Lam2: float
-) -> np.ndarray:
-    """Compute 6PN tidal phase correction
-    Eq.(1,4) phttps://arxiv.org/abs/1310.8288]
-    --------
-    f = frequency series [Hz]
-    M = total mass [solar masses]
-    eta = symmetric mass ratio [dimensionless]
-    s1x = primary spin component along x axis [dimensionless]
-    s1y = primary spin component along y axis [dimensionless]
-    s1z = primary spin component along z axis [dimensionless]
-    s2x = secondary spin component along x axis [dimensionless]
-    s2y = secondary spin component along y axis [dimensionless]
-    s2z = secondary spin component along z axis [dimensionless]
-    Lam1 = primary tidal parameter ell=2 [dimensionless]
-    Lam2 = secondary tidal parameter ell=2 [dimensionless]
-    """
-    v = np.power(np.abs(np.pi * M * f * SUN_MASS_SECONDS), 1.0 / 3.0)
-    v2 = v * v
-    v5 = v ** 5
-    v10 = v ** 10
-    v12 = v10 * v2
-    delta = np.sqrt(1.0 - 4.0 * eta)
-    m1M = 0.5 * (1.0 + delta)
-    m2M = 0.5 * (1.0 - delta)
-    Lam = compute_lambda_tilde(m1M, m2M, Lam1, Lam2)
-    dLam = compute_delta_lambda(m1M, m2M, Lam1, Lam2)
-    LO = 3.0 / 128.0 / eta / v5
-    return LO * (
-        Lam * v10 * (-39.0 / 2.0 - 3115.0 / 64.0 * v2) + dLam * 6595.0 / 364.0 * v12
-    )
-
-
-@njit(cache=True)
 def PhifT7hPN(
     f: np.ndarray, M: float, eta: float, Lama: float, Lamb: float
 ) -> np.ndarray:
@@ -322,144 +287,6 @@ def PhifQM3hPN(
     )  # NLO
     PhifQM += -75.0 / (8.0 * eta) * a2CQ_p_a2CQ * v2 * np.pi  # Tail
     return PhifQM
-
-
-@njit(cache=True)
-def Phif3hPN(
-    f: np.ndarray,
-    M: float,
-    eta: float,
-    s1x: float = 0.0,
-    s1y: float = 0.0,
-    s1z: float = 0.0,
-    s2x: float = 0.0,
-    s2y: float = 0.0,
-    s2z: float = 0.0,
-    Lam: float = 0.0,
-    dLam: float = 0.0,
-) -> np.ndarray:
-    """Compute post-Newtonian phase @ 3.5PN for compact binary coalescences
-    including spins contributions and tidal effects @ 6PN (if Lam or dLam != 0)
-    --------
-    f = frequency series [Hz]
-    M = binary mass [solar masses]
-    s1x = primary spin component along x axis [dimensionless]
-    s1y = primary spin component along y axis [dimensionless]
-    s1z = primary spin component along z axis [dimensionless]
-    s2x = secondary spin component along x axis [dimensionless]
-    s2y = secondary spin component along y axis [dimensionless]
-    s2z = secondary spin component along z axis [dimensionless]
-    Lam = reduced tidal deformability parameter [dimensionless]
-    dLam = asymmetric reduced tidal deformation parameter [dimensionless]
-    --------
-    Adapted from
-    https://bitbucket.org/dailiang8/gwbinning/src/master/
-    """
-    vlso = 1.0 / np.sqrt(6.0)
-    delta = np.sqrt(1.0 - 4.0 * eta)
-    v = np.abs(np.pi * M * f * SUN_MASS_SECONDS) ** (1.0 / 3.0)
-    v2 = v * v
-    v3 = v2 * v
-    v4 = v2 * v2
-    v5 = v4 * v
-    v6 = v3 * v3
-    v7 = v3 * v4
-    v10 = v5 * v5
-    v12 = v10 * v2
-    eta2 = eta ** 2
-    eta3 = eta ** 3
-
-    m1M = 0.5 * (1.0 + delta)
-    m2M = 0.5 * (1.0 - delta)
-    chi1L = s1z
-    chi2L = s2z
-    chi1sq = s1x * s1x + s1y * s1y + s1z * s1z
-    chi2sq = s2x * s2x + s2y * s2y + s2z * s2z
-    chi1dotchi2 = s1x * s2x + s1y * s2y + s1z * s2z
-    SL = m1M * m1M * chi1L + m2M * m2M * chi2L
-    dSigmaL = delta * (m2M * chi2L - m1M * chi1L)
-
-    # Phase correction due to spins
-    sigma = eta * (721.0 / 48.0 * chi1L * chi2L - 247.0 / 48.0 * chi1dotchi2)
-    sigma += 719.0 / 96.0 * (m1M * m1M * chi1L * chi1L + m2M * m2M * chi2L * chi2L)
-    sigma -= 233.0 / 96.0 * (m1M * m1M * chi1sq + m2M * m2M * chi2sq)
-    phis_15PN = 188.0 * SL / 3.0 + 25.0 * dSigmaL
-    ga = (554345.0 / 1134.0 + 110.0 * eta / 9.0) * SL + (
-        13915.0 / 84.0 - 10.0 * eta / 3.0
-    ) * dSigmaL
-    pn_ss3 = (326.75 / 1.12 + 557.5 / 1.8 * eta) * eta * chi1L * chi2L
-    pn_ss3 += (
-        (
-            (4703.5 / 8.4 + 2935.0 / 6.0 * m1M - 120.0 * m1M * m1M)
-            + (-4108.25 / 6.72 - 108.5 / 1.2 * m1M + 125.5 / 3.6 * m1M * m1M)
-        )
-        * m1M
-        * m1M
-        * chi1sq
-    )
-    pn_ss3 += (
-        (
-            (4703.5 / 8.4 + 2935.0 / 6.0 * m2M - 120.0 * m2M * m2M)
-            + (-4108.25 / 6.72 - 108.5 / 1.2 * m2M + 125.5 / 3.6 * m2M * m2M)
-        )
-        * m2M
-        * m2M
-        * chi2sq
-    )
-    phis_3PN = np.pi * (3760.0 * SL + 1490.0 * dSigmaL) / 3.0 + pn_ss3
-    phis_35PN = (
-        -8980424995.0 / 762048.0 + 6586595.0 * eta / 756.0 - 305.0 * eta2 / 36.0
-    ) * SL - (
-        170978035.0 / 48384.0 - 2876425.0 * eta / 672.0 - 4735.0 * eta2 / 144.0
-    ) * dSigmaL
-
-    # Point mass
-    LO = 3.0 / 128.0 / eta / v5
-    # pointmass = 1.0
-    pointmass = (
-        1
-        + 20.0 / 9.0 * (743.0 / 336.0 + 11.0 / 4.0 * eta) * v2
-        + (phis_15PN - 16.0 * np.pi) * v3
-        + 10.0
-        * (3058673.0 / 1016064.0 + 5429.0 / 1008.0 * eta + 617.0 / 144.0 * eta2 - sigma)
-        * v4
-        + (38645.0 / 756.0 * np.pi - 65.0 / 9.0 * eta * np.pi - ga)
-        * (1.0 + 3.0 * np.log(v / vlso))
-        * v5
-        + (
-            11583231236531.0 / 4694215680.0
-            - 640.0 / 3.0 * np.pi ** 2
-            - 6848.0 / 21.0 * (EULER_GAMMA + np.log(4.0 * v))
-            + (-15737765635.0 / 3048192.0 + 2255.0 * np.pi ** 2 / 12.0) * eta
-            + 76055.0 / 1728.0 * eta2
-            - 127825.0 / 1296.0 * eta3
-            + phis_3PN
-        )
-        * v6
-        + (
-            np.pi
-            * (
-                77096675.0 / 254016.0
-                + 378515.0 / 1512.0 * eta
-                - 74045.0 / 756.0 * eta ** 2
-            )
-            + phis_35PN
-        )
-        * v7
-    )
-
-    # Tidal correction to phase at 6PN
-    # Eq.(1,4) [https://arxiv.org/abs/1310.8288]
-    # Lam is the reduced tidal deformation parameter (\tilde\Lambda)
-    # dLam is the asymmetric reduced tidal deformation parameter (\delta\tilde\Lambda)
-    if Lam != 0.0 or dLam != 0.0:
-        tidal = (
-            Lam * v10 * (-39.0 / 2.0 - 3115.0 / 64.0 * v2) + dLam * 6595.0 / 364.0 * v12
-        )
-    else:
-        tidal = 0.0 * v
-
-    return LO * (pointmass + tidal)
 
 
 @njit(cache=True)
@@ -787,269 +614,139 @@ def Af3hPN(
     )
 
 
-# Combine the modulus and the phase of h(f)
-def TaylorF2(
-    phaseorder,
-    f,
-    M,
-    q,
-    s1x=0.0,
-    s1y=0.0,
-    s1z=0.0,
-    s2x=0.0,
-    s2y=0.0,
-    s2z=0.0,
-    Lam1=0.0,
-    Lam2=0.0,
-    Deff=1.0,
-    iota=0.0,
-    f_min=20.0,
-    phiRef=0.0,
-    timeShift=0.0,
-    tidalorder=12,
-    usequadrupolemonopole=0,
-    usenewtides=0,
-):
-    """Compute post-Newtonian frequency-domain waveform @ 3.5PN or 5.5PN for compact binary coalescences,
-    including spins contributions and tidal effects @ 6PN or 7.5PN
-    Maximum frequency is ISCO (identically zero above).
+@njit(cache=True)
+def Phif3hPN(
+    f: np.ndarray,
+    M: float,
+    eta: float,
+    s1x: float = 0.0,
+    s1y: float = 0.0,
+    s1z: float = 0.0,
+    s2x: float = 0.0,
+    s2y: float = 0.0,
+    s2z: float = 0.0,
+    Lam: float = 0.0,
+    dLam: float = 0.0,
+) -> np.ndarray:
+    """Compute post-Newtonian phase @ 3.5PN for compact binary coalescences
+    including spins contributions and tidal effects @ 6PN (if Lam or dLam != 0)
     --------
     f = frequency series [Hz]
-    M = total mass [solar masses]
-    eta = symmetric mass ratio [dimensionless]
+    M = binary mass [solar masses]
     s1x = primary spin component along x axis [dimensionless]
     s1y = primary spin component along y axis [dimensionless]
     s1z = primary spin component along z axis [dimensionless]
     s2x = secondary spin component along x axis [dimensionless]
     s2y = secondary spin component along y axis [dimensionless]
     s2z = secondary spin component along z axis [dimensionless]
-    Lam1 = primary tidal parameter ell=2 [dimensionless]
-    Lam2 = secondary tidal parameter ell=2 [dimensionless]
-    Deff = luminosiry distance [Mpc]
-    iota = inclination angle [rad]
-    Fmin = initial frequency [Hz]
-    phiRef = reference phase [rad]
-    timeShift = time-shift [sec]
-    --------
-    hp = array, comples
-    plus polarization
-    hc = array, comples
-    cross polarization
+    Lam = reduced tidal deformability parameter [dimensionless]
+    dLam = asymmetric reduced tidal deformation parameter [dimensionless]
     --------
     Adapted from
     https://bitbucket.org/dailiang8/gwbinning/src/master/
     """
-    pre = 3.6686934875530996e-19  # (GN*Msun/c^3)^(5/6)/Hz^(7/6)*c/Mpc/sec
-    eta = q / ((1.0 + q) * (1.0 + q))
+    vlso = 1.0 / np.sqrt(6.0)
+    delta = np.sqrt(1.0 - 4.0 * eta)
+    v = np.abs(np.pi * M * f * SUN_MASS_SECONDS) ** (1.0 / 3.0)
+    v2 = v * v
+    v3 = v2 * v
+    v4 = v2 * v2
+    v5 = v4 * v
+    v6 = v3 * v3
+    v7 = v3 * v4
+    v10 = v5 * v5
+    v12 = v10 * v2
+    eta2 = eta ** 2
+    eta3 = eta ** 3
 
-    # Point mass phase
-    # Note Lam = 0 = dLam in the following calls
-    if phaseorder == 7:
-        Phi = Phif3hPN(f, M, eta, s1x, s1y, s1z, s2x, s2y, s2z)
+    m1M = 0.5 * (1.0 + delta)
+    m2M = 0.5 * (1.0 - delta)
+    chi1L = s1z
+    chi2L = s2z
+    chi1sq = s1x * s1x + s1y * s1y + s1z * s1z
+    chi2sq = s2x * s2x + s2y * s2y + s2z * s2z
+    chi1dotchi2 = s1x * s2x + s1y * s2y + s1z * s2z
+    SL = m1M * m1M * chi1L + m2M * m2M * chi2L
+    dSigmaL = delta * (m2M * chi2L - m1M * chi1L)
+
+    # Phase correction due to spins
+    sigma = eta * (721.0 / 48.0 * chi1L * chi2L - 247.0 / 48.0 * chi1dotchi2)
+    sigma += 719.0 / 96.0 * (m1M * m1M * chi1L * chi1L + m2M * m2M * chi2L * chi2L)
+    sigma -= 233.0 / 96.0 * (m1M * m1M * chi1sq + m2M * m2M * chi2sq)
+    phis_15PN = 188.0 * SL / 3.0 + 25.0 * dSigmaL
+    ga = (554345.0 / 1134.0 + 110.0 * eta / 9.0) * SL + (
+        13915.0 / 84.0 - 10.0 * eta / 3.0
+    ) * dSigmaL
+    pn_ss3 = (326.75 / 1.12 + 557.5 / 1.8 * eta) * eta * chi1L * chi2L
+    pn_ss3 += (
+        (
+            (4703.5 / 8.4 + 2935.0 / 6.0 * m1M - 120.0 * m1M * m1M)
+            + (-4108.25 / 6.72 - 108.5 / 1.2 * m1M + 125.5 / 3.6 * m1M * m1M)
+        )
+        * m1M
+        * m1M
+        * chi1sq
+    )
+    pn_ss3 += (
+        (
+            (4703.5 / 8.4 + 2935.0 / 6.0 * m2M - 120.0 * m2M * m2M)
+            + (-4108.25 / 6.72 - 108.5 / 1.2 * m2M + 125.5 / 3.6 * m2M * m2M)
+        )
+        * m2M
+        * m2M
+        * chi2sq
+    )
+    phis_3PN = np.pi * (3760.0 * SL + 1490.0 * dSigmaL) / 3.0 + pn_ss3
+    phis_35PN = (
+        -8980424995.0 / 762048.0 + 6586595.0 * eta / 756.0 - 305.0 * eta2 / 36.0
+    ) * SL - (
+        170978035.0 / 48384.0 - 2876425.0 * eta / 672.0 - 4735.0 * eta2 / 144.0
+    ) * dSigmaL
+
+    # Point mass
+    LO = 3.0 / 128.0 / eta / v5
+    # pointmass = 1.0
+    pointmass = (
+        1
+        + 20.0 / 9.0 * (743.0 / 336.0 + 11.0 / 4.0 * eta) * v2
+        + (phis_15PN - 16.0 * np.pi) * v3
+        + 10.0
+        * (3058673.0 / 1016064.0 + 5429.0 / 1008.0 * eta + 617.0 / 144.0 * eta2 - sigma)
+        * v4
+        + (38645.0 / 756.0 * np.pi - 65.0 / 9.0 * eta * np.pi - ga)
+        * (1.0 + 3.0 * np.log(v / vlso))
+        * v5
+        + (
+            11583231236531.0 / 4694215680.0
+            - 640.0 / 3.0 * np.pi ** 2
+            - 6848.0 / 21.0 * (EULER_GAMMA + np.log(4.0 * v))
+            + (-15737765635.0 / 3048192.0 + 2255.0 * np.pi ** 2 / 12.0) * eta
+            + 76055.0 / 1728.0 * eta2
+            - 127825.0 / 1296.0 * eta3
+            + phis_3PN
+        )
+        * v6
+        + (
+            np.pi
+            * (
+                77096675.0 / 254016.0
+                + 378515.0 / 1512.0 * eta
+                - 74045.0 / 756.0 * eta ** 2
+            )
+            + phis_35PN
+        )
+        * v7
+    )
+
+    # Tidal correction to phase at 6PN
+    # Eq.(1,4) [https://arxiv.org/abs/1310.8288]
+    # Lam is the reduced tidal deformation parameter (\tilde\Lambda)
+    # dLam is the asymmetric reduced tidal deformation parameter (\delta\tilde\Lambda)
+    if Lam != 0.0 or dLam != 0.0:
+        tidal = (
+            Lam * v10 * (-39.0 / 2.0 - 3115.0 / 64.0 * v2) + dLam * 6595.0 / 364.0 * v12
+        )
     else:
-        Phi = Phif5hPN(f, M, eta, s1x, s1y, s1z, s2x, s2y, s2z)
-    # Tidal and QM contributions
-    PhiT = 0.0
-    if Lam1 != 0.0 or Lam2 != 0.0:
-        # Tidal terms
-        if tidalorder != 12 and tidalorder == 15 and usenewtides == 1:
-            PhiT = PhifT7hPNComplete(f, M, eta, Lam1, Lam2)
-        elif tidalorder != 12 and tidalorder == 15:
-            PhiT = PhifT7hPN(f, M, eta, Lam1, Lam2)
-        else:
-            PhiT = PhifT6PN(f, M, eta, Lam1, Lam2)
-        # Quadrupole-monopole term
-        # [https://arxiv.org/abs/gr-qc/9709032]
-        PhiQM = 0.0
-        if usequadrupolemonopole:
-            PhiQM = PhifQM3hPN(f, M, eta, s1x, s1y, s1z, s2x, s2y, s2z, Lam1, Lam2)
-        PhiT += PhiQM
+        tidal = 0.0 * v
 
-    # Shift phase
-    Phi += PhiT + phiRef + 2 * np.pi * f * timeShift
-
-    # Amplitude (no tidal correction implemented)
-    A = Af3hPN(
-        f,
-        M,
-        eta,
-        s1x=s1x,
-        s1y=s1y,
-        s1z=s1z,
-        s2x=s2x,
-        s2y=s2y,
-        s2z=s2z,
-        Lam=0.0,
-        dLam=0.0,
-        Deff=Deff,
-    )
-
-    # h+, hx
-    # Note the convention for the sign in front of the phase
-    cosi = np.cos(iota)
-    incl_plus = (1 + cosi * cosi) * 0.5
-    incl_cross = cosi
-    h = pre * A * np.exp(-1j * Phi)
-    hp = h * incl_plus
-    hc = h * np.exp(-1j * np.pi / 2) * incl_cross
-    return hp, hc
-
-
-def taylorf2_35pn_wrapper(freqs, params):
-    """Wrapper for TF2 3.5PN + (6PN tides) , (if Lam1 or Lam2 != 0)"""
-    phaseorder = 7
-    tidalorder = 12
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-    )
-
-
-def taylorf2_55pn_wrapper(freqs, params):
-    """Wrapper for TF2 5.5PN + (6PN tides) , (if Lam1 or Lam2 != 0)"""
-    phaseorder = 11
-    tidalorder = 12
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-    )
-
-
-def taylorf2_55pn75pntides_wrapper(freqs, params):
-    """Wrapper for TF2 5.5PN + (7.5PN tides) , (if Lam1 or Lam2 != 0)"""
-    phaseorder = 11
-    tidalorder = 15
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-    )
-
-
-def taylorf2_55pn75pnnewtides_wrapper(freqs, params):
-    """Wrapper for TF2 5.5PN + (7.5PN new tides from Henry et al. 2020) , (if Lam1 or Lam2 != 0)"""
-    phaseorder = 11
-    tidalorder = 15
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-        usenewtides=1,
-    )
-
-
-def taylorf2_55pn35pnqm75pntides_wrapper(freqs, params):
-    """Wrapper for TF2 5.5PN + (3.5 PN QM) + (7.5PN tides) , (if Lam1 or Lam2 != 0)"""
-    phaseorder = 11
-    tidalorder = 15
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-        usequadrupolemonopole=1,
-    )
-
-
-def taylorf2_wrapper(freqs, params, phaseorder=11, tidalorder=15, useqm=1):
-    """
-    Wrapper for TF2 (generic)
-    Defaults: 5.5PN + (3.5 PN QM) + (7.5PN tides) , (if Lam1 or Lam2 != 0)
-    """
-    return TaylorF2(
-        phaseorder,
-        freqs,
-        params["mtot"],
-        params["q"],
-        params["s1x"],
-        params["s1y"],
-        params["s1z"],
-        params["s2x"],
-        params["s2y"],
-        params["s2z"],
-        params["lambda1"],
-        params["lambda2"],
-        params["distance"],
-        params["iota"],
-        params["f_min"],
-        params["phi_ref"],
-        0.0,
-        tidalorder=tidalorder,
-        usequadrupolemonopole=useqm,
-    )
+    return LO * (pointmass + tidal)
