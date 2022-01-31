ae9170429240ec79fa3e5b1b71c36f6dc434c921
jacopo <jacopok@gmail.com>
2022-01-31 09:31:48 +0100

    Added RTD search extension

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     "myst_parser",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.viewcode",
+    "sphinx_search.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
