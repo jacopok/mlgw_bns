fixed sign in amplitude and phase recombination

mlgw_bns/model.py
@@ -708,4 +708,4 @@ def combine_amplitude_phase(amp: np.ndarray, phi: np.ndarray) -> np.ndarray:
     np.ndarray
         Cartesian waveform.
     """
-    return amp * np.exp(-1j * phi)
+    return amp * np.exp(1j * phi)