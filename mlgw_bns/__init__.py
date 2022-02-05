838e5b6ef1ce6d05eeb438b6c1b0dab64bbc64e0
jacopo <jacopok@gmail.com>
2022-02-05 19:32:46 +0100

    updated docs in __init__

--- mlgw_bns/__init__.py
+++ mlgw_bns/__init__.py
@@ -1,6 +1,5 @@
-"""Documentation for the ``mlgw_bns`` package.
-
-Its purpose is to speed up the generation of gravitational
+"""
+The purpose of the ``mlgw_bns`` package is to speed up the generation of gravitational
 waveforms for binary neutron star mergers, by training a machine 
 learning model on effective-one-body waveforms.
 
