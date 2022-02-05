4ad535f053794d9b0dac44fa5a950f91a62d2b8f
jacopo <jacopok@gmail.com>
2022-02-05 19:32:02 +0100

    Updated __init__

--- mlgw_bns/__init__.py
+++ mlgw_bns/__init__.py
@@ -1,4 +1,10 @@
 """Documentation for the ``mlgw_bns`` package.
+
+Its purpose is to speed up the generation of gravitational
+waveforms for binary neutron star mergers, by training a machine 
+learning model on effective-one-body waveforms.
+
+The code can be found on the `github page <https://github.com/jacopok/mlgw_bns>`_.
 """
 
 
