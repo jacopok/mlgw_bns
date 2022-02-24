fa2fc1f13bfea600fbe03a5e241a22d1452f1b22
jacopo <jacopok@gmail.com>
2022-02-25 00:31:44 +0100

    New script for making a default dataset

---
+++ make_default_dataset.py
@@ -0,0 +1,14 @@
+from mlgw_bns import *
+
+
+def main():
+    m = Model("default_dataset")
+    m.generate(512, 1 << 14, 1 << 16)
+    m.set_hyper_and_train_nn()
+    m.save()
+
+    # import os; os.replace...
+
+
+if __name__ == "__main__":
+    main()
