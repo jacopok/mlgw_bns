d0b015f7d535349151cc6c1c365f3c5ec72c1bd7
jacopo <jacopok@gmail.com>
2022-03-03 22:29:14 +0100

    Made default dataset go up to regular size

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -3,9 +3,9 @@ from mlgw_bns import *
 
 def main():
     m = Model("default_dataset")
-    m.generate(512, 1 << 14, 1 << 16)
+    m.generate(512, 1 << 15, 1 << 17)
     m.set_hyper_and_train_nn()
-    m.save(False)
+    m.save()
 
     # import os; os.replace...
 
