c2e5a08adc2a8ad7d9f1780c245c45089616aca6
jacopok <jacopok@gmail.com>
2022-03-07 19:33:06 +0100

    bigger default dataset

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -3,9 +3,9 @@ from mlgw_bns import *
 
 def main():
     m = Model("default_dataset")
-    m.generate(512, 1 << 15, 1 << 17)
+    m.generate(1 << 10, 1 << 15, 1 << 16)
     m.set_hyper_and_train_nn()
-    m.save()
+    m.save(False)
 
     # import os; os.replace...
 
