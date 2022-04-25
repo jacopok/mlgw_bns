2c6cb049cb3e18679be8949efe03db87ca67afb3
jacopok <jacopok@gmail.com>
2022-04-25 22:35:59 +0200

    cosmetic

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,3 @@
 def test_imports_for_ndarray_issues():
-    # this test does nothing, but it raises warnings within pytest! 
+    # this test does nothing, but it raises warnings within pytest!
     import pycbc.psd
