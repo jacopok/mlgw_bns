2923cacf5193323b2ee5f2ab19c0b17a67ab9172
jacopok <jacopok@gmail.com>
2022-04-26 17:38:46 +0200

    Documentation and small bugfixes

--- mlgw_bns/higher_order_modes.py
+++ mlgw_bns/higher_order_modes.py
@@ -1,7 +1,19 @@
+r""" This module deals with the generation of waveforms coming from a single mode 
+of a spherical harmonic decomposition of the GW emission from a binary system.
+
+As described in `the TEOBResumS documentation <https://bitbucket.org/eob_ihes/teobresums/wiki/Conventions,%20parameters%20and%20output>`_,
+the full waveform is expressed as 
+
+:math:`h_+ - i h_\times = \sum_{\ell m} A_{\ell m} e^{-i \phi_{\ell m}} Y_{\ell m}(\iota, \varphi)`
+
+where the pair :math:`\ell, m`, with :math:`\ell \geq m`, is known as a *mode*, and
+it is implemented as the namedtuple :class:`Mode`.
+"""
+
 from __future__ import annotations
 
 from collections import namedtuple
-from typing import Callable, Optional
+from typing import Callable, NamedTuple, Optional
 
 import numpy as np
 
@@ -14,9 +26,15 @@ from .dataset_generation import (
 
 WaveformCallable = Callable[[WaveformParameters, np.ndarray], np.ndarray]
 
-Mode = namedtuple("Mode", ["l", "m"])
+# Mode = namedtuple("Mode", ["l", "m"])
+class Mode(NamedTuple):
+    """A mode in the harmonic decomposition of the GW emission from a system."""
 
-# TODO fix these, but it's not so bad now - 
+    l: int
+    m: int
+
+
+# TODO fix these, but it's not so bad now -
 # these are only wrong by a constant scaling
 
 _post_newtonian_amplitudes_by_mode: dict[Mode, WaveformCallable] = {
@@ -37,10 +55,8 @@ class ModeGenerator(WaveformGenerator):
 
         # TODO improve the way these are handled
         self.supported_modes = list(_post_newtonian_amplitudes_by_mode.keys())
-
-    def __post_init__(self):
         if self.mode not in self.supported_modes:
-            raise NotImplementedError(f"Mode {self.mode} is not supported yet!")
+            raise NotImplementedError(f"{self.mode} is not supported yet!")
 
 
 class BarePostNewtonianModeGenerator(ModeGenerator):
@@ -63,7 +79,7 @@ class BarePostNewtonianModeGenerator(ModeGenerator):
         )
 
 
-class EffectiveOneBodyModeGenerator(BarePostNewtonianModeGenerator):
+class TEOBResumSModeGenerator(BarePostNewtonianModeGenerator):
     def __init__(self, eobrun_callable: Callable, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.eobrun_callable = eobrun_callable
@@ -110,14 +126,15 @@ class EffectiveOneBodyModeGenerator(BarePostNewtonianModeGenerator):
 
         f_spa, _, _, _, _, hflm, _, _ = self.eobrun_callable(par_dict)
 
-        amplitude = hflm[mode_to_k(self.mode)][0]
-        phase = hflm[mode_to_k(self.mode)][1]
+        amplitude = hflm[str(mode_to_k(self.mode))][0]
+        phase = hflm[str(mode_to_k(self.mode))][1]
 
         return (f_spa, amplitude, phase)
 
 
-def mode_to_k(mode: Mode):
+def mode_to_k(mode: Mode) -> int:
     """
-    Map (l,m) -> k
+    Map a mode to a unique integer identifier; needed because of
+    TEOBResumS conventions.
     """
     return int(mode.l * (mode.l - 1) / 2 + mode.m - 2)
