e4c2e70fb8d733a2ed51a78f9ca4088d96c2b5f6
jacopok <jacopok@gmail.com>
2022-06-17 00:30:43 +0200

    feat: default dataset from 5Hz

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -2,13 +2,11 @@ from mlgw_bns import *
 
 
 def main():
-    m = Model("default_dataset")
-    m.generate(1 << 10, 1 << 15, 1 << 16)
+    m = Model("default_dataset", initial_frequency_hz=5.)
+    m.downsampling_training.tol = 1.2e-6
+    m.generate(1 << 10, 1 << 14, 1 << 16)
     m.set_hyper_and_train_nn()
     m.save(False)
-
-    # import os; os.replace...
-
 
 if __name__ == "__main__":
     main()
