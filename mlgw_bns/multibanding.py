fix: time-to-merger formula was missing a minus

mlgw_bns/multibanding.py
@@ -63,7 +63,7 @@ def seglen_from_freq(
     eta = maximum_mass_ratio / (1 + maximum_mass_ratio) ** 2
 
     seglen = (
-        SEGLEN_20_HZ * (f_0 / 20) ** (-8 / 3) * (m_tot / 2.8) ** (5 / 3) / (4 * eta)
+        SEGLEN_20_HZ * (f_0 / 20) ** (-8 / 3) * (m_tot / 2.8) ** (-5 / 3) / (4 * eta)
     ) * (1 + margin_percent / 100)
 
     return 2 ** (np.ceil(np.log2(seglen))) if power_of_two else seglen