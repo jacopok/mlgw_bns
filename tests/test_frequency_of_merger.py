713bc5f1e8bb46bca2353ee12dc6ec7f763f305b
jacopok <jacopok@gmail.com>
2022-09-16 15:47:55 +0200

    cosmetic: fixed whitespace

--- tests/test_frequency_of_merger.py
+++ tests/test_frequency_of_merger.py
@@ -2,7 +2,7 @@ from mlgw_bns.frequency_of_merger import frequency_of_merger
 
 
 def test_frequency_of_merger_order_of_magnitude(parameters):
-    
+
     f = frequency_of_merger(parameters)
 
     assert 0.02 < f
