24dc892131c8b7aad0cfe35f2aaa21ccb5c65362
jacopok <jacopok@gmail.com>
2022-09-17 21:59:17 +0200

    feat: increased default dataset training data

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -2,7 +2,7 @@ from mlgw_bns import Model
 
 def main():
     m = Model("default_dataset", initial_frequency_hz=5.)
-    m.generate(1 << 10, 1 << 14, 1 << 16)
+    m.generate(2 ** 10, 2 ** 16, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
 
