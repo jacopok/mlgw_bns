911cf3eb7b0934cda6c4e5187039d3bf1188a53b
jacopok <jacopok@gmail.com>
2022-09-17 18:14:24 +0200

    refactor: cleaned up default dataset making a bit

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -1,11 +1,10 @@
-from mlgw_bns import *
-
+from mlgw_bns import Model
 
 def main():
     m = Model("default_dataset", initial_frequency_hz=5.)
     m.generate(1 << 10, 1 << 14, 1 << 16)
     m.set_hyper_and_train_nn()
-    m.save(False)
+    m.save(include_training_data=False)
 
 if __name__ == "__main__":
     main()
