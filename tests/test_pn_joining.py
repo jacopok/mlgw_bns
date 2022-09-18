80865b5d656b63132d9f6f68423a4545e566d539
jacopok <jacopok@gmail.com>
2022-09-18 19:49:27 +0200

    test: refactored pn joining test

--- tests/test_pn_joining.py
+++ tests/test_pn_joining.py
@@ -7,20 +7,33 @@ from mlgw_bns.model_validation import ValidateModel
 
 from .test_model import random_parameters
 
+PHI_TOL = 1e-6
+LOG_AMP_TOL = 1e-2
 
-@pytest.mark.xfail
+
 @pytest.mark.requires_default
-def test_default_model_pn_connection(default_model):
+@pytest.mark.parametrize("mass", [2.0, 2.8, 4.0])
+def test_default_model_pn_connection(default_model, mass):
     par = random_parameters(default_model, 1)
-    par.total_mass = 2.4
+    par.total_mass = mass
 
-    f_low = default_model.dataset.effective_initial_frequency_hz * (2.8 / 2.4)
+    f_low = default_model.dataset.effective_initial_frequency_hz * (2.8 / mass)
 
     freqs = np.linspace(f_low - 1e-4, f_low + 1e-4, num=2000)
 
     with pytest.warns(UserWarning):
         hp, hc = default_model.predict(freqs, par)
 
-    assert np.isclose(abs(hp)[999], abs(hp)[1001], atol=0, rtol=0.05)
+    hp_gradient_aroud_f_low = np.ediff1d(np.log(abs(hp))[980:1005])
+    avg_hp_gradient = np.average(hp_gradient_aroud_f_low[:18])
 
-    assert np.isclose(np.angle(hp)[999], np.angle(hp)[1001], atol=0.01, rtol=0)
+    assert np.allclose(
+        hp_gradient_aroud_f_low, avg_hp_gradient, rtol=0, atol=LOG_AMP_TOL
+    )
+
+    unwrapped_phase_around_f_low = np.unwrap(np.angle(hp)[980:1005])
+    delta_phi_between_points = np.ediff1d(unwrapped_phase_around_f_low)
+
+    avg_delta_phi = np.average(delta_phi_between_points[:18])
+
+    assert np.allclose(delta_phi_between_points, avg_delta_phi, rtol=0, atol=PHI_TOL)
