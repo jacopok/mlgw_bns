01f58b45709be36c02cb314cb15fd4e630ec0c57
jacopok <jacopok@gmail.com>
2022-09-19 12:53:05 +0200

    feat: code to generate a new default dataset

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -1,10 +1,18 @@
 from mlgw_bns import Model
 
-def main():
+def main_model():
     m = Model("default_dataset", initial_frequency_hz=5.)
     m.generate(2 ** 10, 2 ** 16, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
 
+def model_2():
+
+    m = Model("default_dataset_faster", initial_frequency_hz=15.)
+    m.generate(2 ** 10, 2 ** 16, 2 ** 17)
+    m.set_hyper_and_train_nn()
+    m.save(include_training_data=False)
+
 if __name__ == "__main__":
-    main()
+    # main_model()
+    model_2()
