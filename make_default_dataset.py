e3faf96afd3c528fba69bd8d64535446eafe39dc
jacopok <jacopok@gmail.com>
2022-09-19 14:50:24 +0200

    feat: reduced PC number in fast model

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -8,7 +8,7 @@ def main_model():
 
 def model_2():
 
-    m = Model("default_dataset_faster", initial_frequency_hz=15.)
+    m = Model("default_dataset_faster", initial_frequency_hz=15., pca_components_number=25)
     m.generate(2 ** 10, 2 ** 16, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
