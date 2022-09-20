bcf9c4387e0cd56b1d9f8beab847b6cc56c3277c
jacopok <jacopok@gmail.com>
2022-09-20 10:45:49 +0200

    feat: new "fast" model

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -1,14 +1,13 @@
 from mlgw_bns import Model
 
 def main_model():
-    m = Model("default_dataset", initial_frequency_hz=5.)
+    m = Model("default", initial_frequency_hz=5.)
     m.generate(2 ** 10, 2 ** 16, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
 
 def model_2():
-
-    m = Model("default_dataset_faster", initial_frequency_hz=15., pca_components_number=25)
+    m = Model("fast", initial_frequency_hz=15.)
     m.generate(2 ** 10, 2 ** 16, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
