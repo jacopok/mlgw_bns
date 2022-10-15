f952cfb894631de8bb01945a55f2a9be48e50d97
jacopok <jacopok@gmail.com>
2022-10-15 12:44:31 +0200

    test: hyperparameter optimization smoketest

--- tests/test_hyperparameter_optimization.py
+++ tests/test_hyperparameter_optimization.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from mlgw_bns.hyperparameter_optimization import HyperparameterOptimization
 
 
@@ -6,3 +8,11 @@ def test_study_creation(generated_model):
 
     assert ho.training_data_number == 100
     assert ho.study_filename == "test_model_study.pkl"
+
+
+def test_optimization_smoketest(generated_model):
+    ho = HyperparameterOptimization(generated_model)
+
+    ho.optimize(timeout_min=1 / 60)
+
+    assert ho.total_training_time() > timedelta(seconds=1)
