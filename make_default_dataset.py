ce5772899faf2ec9c0da264d95a17b60c8353d04
jacopok <jacopok@gmail.com>
2026-08-25 11:06:22 +0200

    generate ModesModel

--- make_default_dataset.py
+++ make_default_dataset.py
@@ -1,8 +1,12 @@
 from mlgw_bns.model import Model
+from mlgw_bns.modes_model import ModesModel
+from mlgw_bns.higher_order_modes import Mode
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def main_model():
     m = Model("default", initial_frequency_hz=5.)
-    m.generate(2 ** 10, 2 ** 16, 2 ** 17)
+    m.generate(2 ** 10, 2 ** 13, 2 ** 17)
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
 
@@ -12,6 +16,17 @@ def model_2():
     m.set_hyper_and_train_nn()
     m.save(include_training_data=False)
 
+def modes_model():
+    m = ModesModel(
+        modes=[Mode(2, 2), Mode(2, 1), Mode(3, 3), Mode(4, 4)],
+        filename="default_hom",
+        initial_frequency_hz=5.,
+    )
+    m.generate(2 ** 8, 2 ** 8, 2 ** 8)
+    m.set_hyper_and_train_nn()
+    m.save(include_training_data=False)
+
 if __name__ == "__main__":
     # main_model()
-    model_2()
+    # model_2()
+    modes_model()
